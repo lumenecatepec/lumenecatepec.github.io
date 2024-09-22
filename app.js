1ba3e9f7b681b4498ce30e4e5c1785a34cc99184
ulisesGez <182389456+lumenecatepec@users.noreply.github.com>
2024-09-22 00:41:48 -0600

    email

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const shareButtons = document.querySelectorAll('.share-button, .tile-share-button')
 console.log(shareButtons)
 
+document.addEventListener('DOMContentLoaded', () => {
+    const emailLink = document.getElementById('email');
+
+    emailLink.addEventListener('click', async (event) => {
+        event.preventDefault(); // Prevenir la acción por defecto del enlace
+
+        const email = 'impecatepec@lumen.com.mx';
+        const subject = 'envio';
+
+        const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}`;
+
+        // Redirigir al enlace mailto
+        window.location.href = mailtoLink;
+    });
+});
+
 async function copyText(e){
     //prevent button going to the site
     e.preventDefault()
